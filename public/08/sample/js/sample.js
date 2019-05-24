d7ebba0695a47991e8c59d865338a6f7505cd56c
romdays <seasonroid@gmail.com>
2019-05-24 14:52:42 +0900

    done 08

--- public/08/sample/js/sample.js
+++ public/08/sample/js/sample.js
@@ -1,20 +1,73 @@
-var svgWidth = 320;
-var svgHeight = 240;
-var dataSet = [50,30, 12, 5, 3]
+var color = d3.scale.category10();
 
-var pie = d3.layout.pie()
+drawPie("mydata2008.csv");
+d3.select("#year").on("change", function(d,i){
+    d3.select("#myGraph").selectAll("*").remove();
+    drawPie("mydata"+this.value+".csv", this.value);
+})
 
-var arc = d3.svg.arc().innerRadius(0).outerRadius(100);
+function drawPie(filename, year){
+    d3.csv(filename, function(error, data){
+        var dataSet = [];
+        for(var i in data[0]){
+            dataSet.push(data[0][i])
+        }
+    
+    var svgEle = document.getElementById("myGraph");
+    var svgWidth = window.getComputedStyle(svgEle, null).getPropertyValue("width");
+    var svgHeight = window.getComputedStyle(svgEle, null).getPropertyValue("height");
+    svgWidth = parseFloat(svgWidth);
+    svgHeight = parseFloat(svgHeight);
 
-var pieElements = d3.select("#myGraph")
-    .selectAll("path")
-    .data(pie(dataSet))
+    var pie = d3.layout.pie()
 
-pieElements.enter()
-    .append("path")
-    .attr({
-        class: "pie",
-        d: arc,
-        transform:
-            "translate("+svgWidth/2+","+svgHeight/2+")"
+    var arc = d3.svg.arc().innerRadius(30).outerRadius(100);
+
+    var pieElements = d3.select("#myGraph")
+        .selectAll("g")
+        .data(pie(dataSet))
+        .enter()
+        .append("g")
+        .attr("transform", "translate("+svgWidth/2+","+svgHeight/2+")")
+
+    pieElements
+        .append("path")
+        .attr({
+            class: "pie",
+            d: arc
+        })
+        .style("fill", function(d, i){
+            return color(i);ß
+        })
+        .transition()
+        .ease("linear")
+        .duration(200)
+        .delay(function(d,i){
+            return i*200;
+        })
+        .attrTween("d", function(d,i){
+            var interpolate =d3.interpolate(
+                {startAngle: d.startAngle, endAngle: d.startAngle},
+                {startAngle: d.startAngle, endAngle: d.endAngle}
+            );
+            return function(t){
+                return arc(interpolate(t));
+            }
+        })
+
+    var textElements = d3.select("#myGraph")
+        .append("text")
+        .attr("class", "total")
+        .attr("transform", "translate("+svgWidth/2+","+(svgHeight/2+5)+")")
+        .text("合計:"+d3.sum(dataSet))
+
+    pieElements.append("text")
+        .attr("class", "pieNum")
+        .attr("transform", function(d,i){
+            return "translate("+arc.centroid(d)+")";
+        })
+        .text(function(d,i){
+            return d.value;
+        })
     })
+}
